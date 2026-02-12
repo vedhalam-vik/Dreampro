07f09446e0bf811b8550e91bfd5e2e9996e95581
vedhalam-vik <153629693+vedhalam-vik@users.noreply.github.com>
2026-02-12 23:02:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: "/Dreampro/",
     const env = loadEnv(mode, '.', '');
     return {
       server: {
